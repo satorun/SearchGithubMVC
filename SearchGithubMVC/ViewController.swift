7303a0d9cc1f57564655949c9a36766b969c0c04
satorun <satorun.org@gmail.com>
2018-09-17 17:39:17 +0900

    Add files

---
+++ SearchGithubMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchGithubMVC
+//
+//  Created by satorun on 2018/09/17.
+//  Copyright © 2018年 satorun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
